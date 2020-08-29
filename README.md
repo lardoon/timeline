fa48f37a6bbd00c91e8feec14e7da0cbbe302f8e
lardoon <petewzone-github@yahoo.com>
2020-08-29 18:46:03 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ivy-tg5k9a
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-tg5k9a)
---
+++ src/app/app.component.ts
@@ -0,0 +1,29 @@
+import { Component, VERSION } from '@angular/core';
+
+import { Midi } from '@tonejs/midi';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
+})
+export class AppComponent  {
+
+  midi: Midi;
+
+  async getFile(event) {
+    this.midi = await this.readMidi(event.files[0]);
+    
+  }
+
+   readMidi(file: Blob) : Promise<Midi> {
+    let reader = new FileReader();
+    return new Promise((resolve, reject) => {
+      reader.onload = (ev) => {
+        resolve(new Midi(<ArrayBuffer> ev.target.result));
+      }
+      reader.onerror = reject;
+    });
+    reader.readAsArrayBuffer(file);
+  }
+}
